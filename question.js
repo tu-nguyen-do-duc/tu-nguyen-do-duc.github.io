01b3c62a6717afd6a5f055e9babd1725740d65f8
tu-nguyen-do-duc <145394618+tu-nguyen-do-duc@users.noreply.github.com>
2023-12-14 04:50:33 +0200

    Add files via upload

---
+++ question.js
@@ -0,0 +1,6 @@
+function submit() {
+    var array = ['arnold.jpg','ronnie.jpg','phil.jpg','jay.jpg','mike.jpg'];
+    var index = Math.round(Math.random()*4);
+    var output = "<img src=" + array[index] + " class='dwarf-image' alt='Customized dwarf'> <br>" + "<h1>This is the epic dwarf you created! Congratulation!</h1>"
+    document.getElementById('output').innerHTML = output;
+}
